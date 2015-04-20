cde174061f4c67396de15d8d1f6a6ba2d3c3fe94
eugeniax <eugenia.xu@gmail.com>
2015-04-20 16:34:00 +0800

    initial app, running

--- server.R
+++ server.R
@@ -3,8 +3,8 @@ library(googleVis)
 #suppressPackageStartupMessages(require(googleVis))
 
 topcountries <- read.csv("data/topcountries2014.csv", header=F, skip=2,
-                         colClasses=c("factor",rep("character",3),rep("numeric",2)), 
-                         col.names=c("rank","country","stu13","stu14","percent","change"))
+                         colClasses=c("factor",rep("character",3),rep("numeric",3)), 
+                         col.names=c("Rank","Country","prevSt","Students","Percent","Change","Year"))
 topcountries[,3:4] <- lapply(topcountries[,3:4],function(x){as.numeric(gsub(",", "", x))})
 
 shinyServer(
@@ -20,11 +20,11 @@ shinyServer(
         ## draw map
         output$gvmap <- renderGvis({
             gvisGeoChart(topcountries,
-                         locationvar="country", colorvar="stu14",
+                         locationvar="Country", colorvar="Students",
                          options=list(region="world", displayMode="regions", 
                                       resolution="countries",
                                       width=800, height=600,
-                                      colorAxis="{colors:['#91BFDB', '#FC8D59']}"
+                                      colorAxis="{colors:['#ece7f2', '#a6bddb', '#2b8cbe']}"
                          ))     
         })
         
