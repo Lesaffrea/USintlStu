8ff00ac4b80e22a6a100603ef94ea525baf6a735
eugeniax <eugenia.xu@gmail.com>
2015-04-25 00:08:55 +0800

    line chart w all countries added

--- server.R
+++ server.R
@@ -1,17 +1,25 @@
 library(shiny)
 library(googleVis)
+library(reshape2)
 #suppressPackageStartupMessages(require(googleVis))
 
 files <- list.files("data", full.names=T)
 topcountries <- data.frame()
 for (i in 1:length(files)) {
     data <- read.csv(files[i], header=F, skip=2,
-                             colClasses=c("factor",rep("character",3),rep("numeric",3)), 
+                             colClasses=c("factor",rep("character",3),rep("numeric",2),"factor"), 
                              col.names=c("Rank","Country","prevSt","Students","Percent","Change","Year"))
     topcountries <- rbind(topcountries, data)
 }
 
 topcountries[,3:4] <- lapply(topcountries[,3:4],function(x){as.numeric(gsub(",", "", x))})
+## standardize country names in prepare for mapping
+topcountries$Country <- gsub("China.*", "China", topcountries$Country)
+topcountries$Country <- gsub("Hong Kong.*", "Hong Kong", topcountries$Country)
+topcountries$Country <- gsub("^Korea.*", "South Korea", topcountries$Country)
+
+## list the countries that have always been on the top list 
+toplist <- unique(topcountries$Country)
 
 shinyServer(
     function(input, output) {
@@ -31,17 +39,41 @@ shinyServer(
                          options=list(region="world", displayMode="regions", 
                                       resolution="countries",
                                       width=800, height=600,
-                                      colorAxis="{colors:['#ece7f2', '#a6bddb', '#2b8cbe']}"
+                                      colorAxis="{colors:['#feb24c', '#fc4e2a', '#b10026']}"
                          ))     
         })
         
-        output$temp <- renderTable({
-            head(topcountries)
+        output$top10tab <- renderTable({
+            myData <- subset(topcountries, Year==myYear())
+            head(myData, 10)
+        })
+        
+        # Create the checkboxes and select them all by default
+        output$countrybox <- renderUI({
+        checkboxGroupInput("countries", 
+                           label = "Choose the countries of origin", 
+                           choices = toplist,
+                           selected = toplist[1:3])
+        })
+        
+        countryList <- reactive ({
+            input$countries
         })
         
         output$countries <- renderText({ 
             paste("You have chosen",
                   input$countries)
+        })
+                
+        output$lines <- renderGvis({
+            linedata <- topcountries[,c("Country","Students","Year")]
+            linedata <- melt(linedata, id.var=c("Year", "Country"))
+            linedata <- dcast(linedata, Year ~ Country, value.var="value")
+            gvisLineChart(linedata,
+                          options=list(width=800, height=800)
+                          
+            )
+            
         })
         
     }
--- ui.R
+++ ui.R
@@ -7,19 +7,17 @@ shinyUI(fluidPage(
             helpText("Where do the international students in US come from?"),
             sliderInput("year", 
                         label = "Choose a year to display", 
-                        min=2011, max=2014, value=2014,  step=1, animate=TRUE),
+                        min=2001, max=2014, value=2014,  step=1, animate=TRUE),
             
             helpText("How has the number of international students evolved over last 10 years?"),
-            checkboxGroupInput("countries", 
-                label = "Choose the countries of origin", 
-                choices = list("China" = 1, "India" = 2, "Vietnam" = 3),
-                        selected = 1)
+            uiOutput("countrybox")
         ),
         mainPanel(
             h3(textOutput("gvisTitle")),    
             htmlOutput("gvmap"),            
-            htmlOutput("temp"),
-            textOutput("countries")
+            htmlOutput("top10tab"),
+            textOutput("countries"),
+            htmlOutput("lines")
         )        
     )    
 ))
