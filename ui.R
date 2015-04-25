ff2b0f54ef01aa9b5fcff882017654d61961a1ae
eugeniax <eugenia.xu@gmail.com>
2015-04-26 03:41:10 +0800

    minor modification on helptext

--- ui.R
+++ ui.R
@@ -4,12 +4,13 @@ shinyUI(fluidPage(
     titlePanel("Trends of International Students in US"),
     sidebarLayout(
         sidebarPanel(
-            helpText("Where do the international students in US come from?"),
+            h4("Where do the international students in US come from?"),
+            helpText("Move the slider to choose the year for display. \n Click the play button to show animation."),
             sliderInput("year", 
                         label = "Choose a year to display", 
                         min=2001, max=2014, value=2014,  step=1, animate=TRUE),
             
-            helpText("How has the number of international students evolved over last 10 years?"),
+            h4("How has the number of international students evolved over last 10 years?"),
             uiOutput("countrybox")
         ),
         mainPanel(
@@ -19,6 +20,7 @@ shinyUI(fluidPage(
 #             htmlOutput("temp"),
 #             htmlOutput("temp2"),
             h3("Trend of International Students by Country"),
+            helpText("Tick the checkboxes on the left to select the countries for plotting lines."),
             htmlOutput("lines"),
             htmlOutput("countrytab")
         )        
